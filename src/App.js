0c92068101f720eda384e3ffdbdc8504206137d5
vp-6599 <vaibhavpachore25@gmail.com>
2024-08-22 19:46:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-//import Counter from './Components/Counter';
-//import TempControl from './Components/TempControl'
-import Todo from './Components/Todo'
- //import Todoslist from './Components/Todoslist'
- //import OnChange from './Components/OnChange'
+import TempControl from './Components/TempControl'
+
 function App() {
   return (
     <div className="App">
-      {/* <Counter/> */}
-      {/* <TempControl/> */ }
-      { <Todo/> }
-      {/*<Todoslist/>*/ }
-      { /*<OnChange/> */ }
+     
+      { <TempControl/> }
+      
     </div>
   );
 }
